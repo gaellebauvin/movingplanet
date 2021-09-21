99b47f7b5fd58272fbd9a1d68d2cf7b48ffcfaee
Tristan Defachel <defachel.tristan@gmail.com>
2021-09-21 16:52:17 +0200

    add function for "playSynth"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
-document.getElementById('undo-btn')?.addEventListener('click', async () => {
+document.addEventListener("DOMContentLoaded", function () {
     const synth = new Tone.Synth().toDestination();
     const now = Tone.now()
-    synth.triggerAttackRelease("C4", "8n", now);
-    synth.triggerAttackRelease("G4", "8n", now + 0.5);
-    synth.triggerAttackRelease("A4", "8n", now + 1);
-    synth.triggerAttackRelease("G2", "8n", now + 2);
-    synth.triggerAttackRelease("E4", "8n", now + 3);
-    synth.triggerAttackRelease("B1", "8n", now + 4);
-    synth.triggerAttackRelease("G2", "8n", now + 5);
-    synth.triggerAttackRelease("A3", "8n", now + 5.5);
-})
+
+    //PLay note with Synth
+    function playSynth(note, duration, time) {
+        synth.triggerAttackRelease(note, duration, time)
+    }
+
+    //OnClick undo button
+    document.getElementById('undo-btn')?.addEventListener('click', async () => {
+        playSynth("C4", "4n", now);
+        playSynth("G4", "4n", now + 0.5);
+        playSynth("A4", "4n", now + 1);
+        playSynth("G2", "4n", now + 2);
+        playSynth("G2", "4n", now + 3);
+        playSynth("B4", "4n", now + 4);
+        playSynth("C4", "4n", now + 5);
+        playSynth("C3", "4n", now + 5.5);
+    })
+});
