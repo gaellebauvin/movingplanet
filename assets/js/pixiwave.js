dd11c017ef275132f80c789911879c814d5b6591
aissatou <aissatou.dia@mail-ecv.fr>
2021-09-22 23:23:22 +0200

    feat: stars in the sky

--- assets/js/pixiwave.js
+++ assets/js/pixiwave.js
@@ -2,7 +2,7 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 
 const app = new PIXI.Application({
-    width: width, height: height,
+    width: width, height: height/2,
     transparent: true,
     resolution: window.devicePixelRatio || 1,
     autoResize:true,
@@ -10,7 +10,6 @@ const app = new PIXI.Application({
 
 app.view.style.width = width+'px';app.view.style.height = height+'px';
 app.view.originWidth = width;app.view.originHeigh = height;
-
 
 document.body.appendChild(app.view);
 
@@ -29,13 +28,12 @@ const strip = new PIXI.SimpleRope(PIXI.Texture.from('assets/images/lines-2.svg')
 strip.x = -459;
 
 const waveContainer = new PIXI.Container();
-// position dans le canva
-//waveContainer.x = 370;
-//waveContainer.y = 300;
 
 // Move container to the center
+//waveContainer.x = app.screen.width / 2;
+//waveContainer.y = app.screen.height / 2;
 waveContainer.x = app.screen.width / 2;
-waveContainer.y = app.screen.height / 2;
+waveContainer.y = app.screen.height / 3.5;
 
 //Set the scale container
 waveContainer.scale.set((app.screen.width +400)/ 1000);
@@ -44,10 +42,10 @@ app.stage.addChild(waveContainer);
 waveContainer.addChild(strip);
 
 app.ticker.add(() => {
-    count += 0.1;
+    count += 0.020;
     // make the waves
     for (let i = 0; i < points.length; i++) {
-        points[i].y = Math.sin((i * 0.7) + count) * 30;
+        points[i].y = Math.sin((i * 0.5) + count) * 30;
         points[i].x = i * ropeLength + Math.cos((i * 0.2) + count) * 20;
     }
 });
