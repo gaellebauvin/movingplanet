74cd7f4d50050106af426e3b375b1026fa0ee7ed
gaelle bauvin <gbauvin@gmail.com>
2021-09-23 14:06:24 +0200

    fix deplacement des planetes + ajout d'un entier pour les notes de musiques

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-$('.planet').on('mouseup', function (){
-    if ($(this).css('inset') >= "0px 0px 0px 0px" && $(this).css('inset') <= "50px 50px 50px 50px"){
-        //la planete est bien placée
-    }
-})
 $(document).ready(function(){
     var planets = $('.planet');
     $.each(planets, function (index, div) {
@@ -11,6 +6,35 @@ $(document).ready(function(){
 
         $(this).css({"inset" : randomX + "px auto auto " + randomY + "px"});
     })
+
+    $('.planet').on('mouseup', function (){
+        var arrayTop = $(this).css('top').split("p");
+        var top = arrayTop[0];
+        var arrayLeft = $(this).css('left').split("p");
+        var left = arrayLeft[0];
+        if (-50 <= top && top <= 50 && -50 <= left && left <= 50) {
+        //la planete est bien placée
+            console.log("bien placé");
+        }  else {
+        // Tant qu'on a pas trouvé dans quel interval se trouve la planète on continue, quand on a trouvé on lui donne un chiffre ( variable entier)
+           var i = false;
+           var entier = 1;
+           var difference = 51;
+
+           while( i !== true ){
+               if( -difference <= top && top <= difference && -difference <= left && left <= difference){
+                   console.log("pas bien place", entier )
+                   i = true;
+               } else {
+                   entier = entier + 1;
+                   difference = difference + 100;
+               }
+           }
+        }
+    })
 });
 
-$('.planet').draggable();
+$('.planet').draggable({
+    containment: "document",
+    scroll: false,
+});
